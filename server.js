9ff9c93fff25c5e5a739d8fb7230af47b17b8e7d
Cobraaz <59387534+Cobraaz@users.noreply.github.com>
2020-01-26 17:38:05 +0530

    adding port

--- server.js
+++ server.js
@@ -11,10 +11,10 @@ const profile = require('./controllers/profile');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : '1234',
-    database : 'smart-brain'
+    host: '127.0.0.1',
+    user: 'postgres',
+    password: '1234',
+    database: 'smart-brain'
   }
 });
 
@@ -23,22 +23,22 @@ const app = express();
 app.use(cors())
 app.use(bodyParser.json());
 
-app.get('/', (req, res)=> { res.send(database.users)})
-app.post('/signin',(req, res) => {signin.handleSignin(req, res, db, bcrypt) } )
-app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt) } )
-app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db) })
+app.get('/', (req, res) => { res.send(database.users) })
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
 
 app.put('/image', (req, res) => {
   const { id } = req.body;
   db('users').where('id', '=', id)
-  .increment('entries', 1)
-  .returning('entries')
-  .then(entries => {
-    res.json(entries[0]);
-  })
-  .catch(err => res.status(400).json('unable to get entries'))
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+      res.json(entries[0]);
+    })
+    .catch(err => res.status(400).json('unable to get entries'))
 })
 
-app.listen(3000, ()=> {
-  console.log('app is running on port 3000');
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`app is running on port ${process.env.PORT} `);
 })
